6bb07809530525fab73e07dfe56c26252dd0d033
Lorenz Brun <lorenz@brun.one>
2023-11-24 14:51:08 +0100

    ethtool: add support for private flags (#22)
    This adds support for geting and setting private flags which are
    driver-specific (and sometimes even model-specific) controls not part of
    any generic network settings API.
    
    They are used to toggle low-level features in various NICs.

--- client_others.go
+++ client_others.go
@@ -16,19 +16,22 @@ func (*Error) Is(_ error) bool { return false }
 
 type client struct{}
 
-func newClient() (*client, error)                           { return nil, errUnsupported }
-func (c *client) LinkInfos() ([]*LinkInfo, error)           { return nil, errUnsupported }
-func (c *client) LinkInfo(_ Interface) (*LinkInfo, error)   { return nil, errUnsupported }
-func (c *client) LinkModes() ([]*LinkMode, error)           { return nil, errUnsupported }
-func (c *client) LinkMode(_ Interface) (*LinkMode, error)   { return nil, errUnsupported }
-func (c *client) LinkStates() ([]*LinkState, error)         { return nil, errUnsupported }
-func (c *client) LinkState(_ Interface) (*LinkState, error) { return nil, errUnsupported }
-func (c *client) WakeOnLANs() ([]*WakeOnLAN, error)         { return nil, errUnsupported }
-func (c *client) WakeOnLAN(_ Interface) (*WakeOnLAN, error) { return nil, errUnsupported }
-func (c *client) SetWakeOnLAN(_ WakeOnLAN) error            { return errUnsupported }
-func (c *client) FEC(_ Interface) (*FEC, error)             { return nil, errUnsupported }
-func (c *client) SetFEC(_ FEC) error                        { return errUnsupported }
-func (c *client) Close() error                              { return errUnsupported }
+func newClient() (*client, error)                                 { return nil, errUnsupported }
+func (c *client) LinkInfos() ([]*LinkInfo, error)                 { return nil, errUnsupported }
+func (c *client) LinkInfo(_ Interface) (*LinkInfo, error)         { return nil, errUnsupported }
+func (c *client) LinkModes() ([]*LinkMode, error)                 { return nil, errUnsupported }
+func (c *client) LinkMode(_ Interface) (*LinkMode, error)         { return nil, errUnsupported }
+func (c *client) LinkStates() ([]*LinkState, error)               { return nil, errUnsupported }
+func (c *client) LinkState(_ Interface) (*LinkState, error)       { return nil, errUnsupported }
+func (c *client) WakeOnLANs() ([]*WakeOnLAN, error)               { return nil, errUnsupported }
+func (c *client) WakeOnLAN(_ Interface) (*WakeOnLAN, error)       { return nil, errUnsupported }
+func (c *client) SetWakeOnLAN(_ WakeOnLAN) error                  { return errUnsupported }
+func (c *client) FEC(_ Interface) (*FEC, error)                   { return nil, errUnsupported }
+func (c *client) SetFEC(_ FEC) error                              { return errUnsupported }
+func (c *client) AllPrivateFlags() ([]*PrivateFlags, error)       { return nil, errUnsupported }
+func (c *client) PrivateFlags(_ Interface) (*PrivateFlags, error) { return nil, errUnsupported }
+func (c *client) SetPrivateFlags(_ PrivateFlags) error            { return errUnsupported }
+func (c *client) Close() error                                    { return errUnsupported }
 
 func (f *FEC) Supported() FECModes { return 0 }
 
